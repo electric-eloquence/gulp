71170affacb2d6e4acae4fff826d7a1e3fa8e148
e2tha-e <ee@electric-eloquence.io>
2020-03-27 08:51:42 -0400

    increased timeout in test

--- test/orchestrator/taskTimings.js
+++ test/orchestrator/taskTimings.js
@@ -14,7 +14,7 @@ describe('orchestrator', function() {
       var timeout;
 
       // Arrange
-      timeout = 0.1; // seconds
+      timeout = 0.2; // seconds
       a = 0;
       fn = function(cb) {
         setTimeout(function() {
@@ -28,7 +28,7 @@ describe('orchestrator', function() {
 
       orchestrator.on('task_stop', function(args) {
         // Assert
-        args.duration.should.be.approximately(timeout, 0.1);
+        args.duration.should.be.approximately(timeout, 0.2);
         args.duration.should.be.above(0);
         ++a;
       });
